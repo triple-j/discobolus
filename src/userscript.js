9ab0b83c661cf2245f2696d2bead38fd983b13e1
Jeremie J. Jarosh <jeremie@jarosh.org>
2019-06-15 11:58:11 -0500

    Add files to DOM via ObjectURLs

--- src/userscript.js
+++ src/userscript.js
@@ -3,7 +3,7 @@
 // @author        Jeremie Jarosh
 // @namespace     http://trejeraos.com/
 // @description   A third party add-on for the web site dcbservice.com
-// @version       0.1.2
+// @version       0.1.3-rc
 // @include       http://dcbservice.com/*
 // @include       http://www.dcbservice.com/*
 // @include       https://dcbservice.com/*
@@ -20,6 +20,22 @@ var elmScript;
 var stylesDataUrl = "{{data:text/css;charset=utf-8;file,../dist/main.css}}";
 var scriptDataUrl = "{{data:text/javascript;charset=utf-8;file,../dist/main.js}}";
 
+function appendToHead(elm, attr, dataUrl) {
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", dataUrl, true);
+    oReq.responseType = "blob";
+
+    oReq.onload = function(oEvent) {
+        var blob = oReq.response;
+        var objectURL = URL.createObjectURL(blob);
+
+        elm.setAttribute(attr, objectURL);
+        elmHead.appendChild(elm);
+    };
+
+    oReq.send();
+}
+
 // add Class to html for easier css overrides
 elmHtml.classList.add(htmlClass);
 
@@ -28,12 +44,14 @@ elmStyle = document.createElement("link");
 elmStyle.setAttribute('type', "text/css");
 elmStyle.setAttribute('rel', "stylesheet");
 elmStyle.setAttribute('title', htmlClass);
-elmStyle.setAttribute('href', stylesDataUrl);
-elmHead.appendChild(elmStyle);
+//elmStyle.setAttribute('href', stylesDataUrl);
+//elmHead.appendChild(elmStyle);
+appendToHead(elmStyle, "href", stylesDataUrl);
 
 // add javascript to the head
 elmScript = document.createElement("script");
 elmScript.setAttribute('type','text/javascript');
 elmScript.setAttribute('title', htmlClass);
-elmScript.setAttribute('src', scriptDataUrl);
-elmHead.appendChild(elmScript);
+//elmScript.setAttribute('src', scriptDataUrl);
+//elmHead.appendChild(elmScript);
+appendToHead(elmScript, "src", scriptDataUrl);
