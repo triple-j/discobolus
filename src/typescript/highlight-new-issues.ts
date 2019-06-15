0600d2fc24fff45234955fa37d2f8a7b30be1382
Jeremie J. Jarosh <jeremie@jarosh.org>
2019-06-14 19:33:35 -0500

    Add `highlightNewIssues()` to global scope

--- src/typescript/highlight-new-issues.ts
+++ src/typescript/highlight-new-issues.ts
@@ -1,26 +1,31 @@
-//.thumblist .detail h5
+export function highlightNewIssues() {
+    // TODO: apply to `TextView` as well
 
-//ul.thumblist li
-//document.querySelectorAll("ul.thumblist li").forEach(console.log.bind(console))
-document.querySelectorAll("ul.thumblist li").forEach(function (item) {
-    let comicTitle = item.querySelector(".detail h5").textContent
-    let comicTitleWords = comicTitle.split(" ")
-    let issueNumbers = comicTitleWords.filter((word) => {
-        //return word.startsWith("#0") || word.startsWith("#1")
-        if (word === "#0" || word === "#1") {
-            return true
-        } else if (word.startsWith("#0") || word.startsWith("#1")) {
-            return /\D/.test(word[2])
+    document.querySelectorAll("ul.thumblist").forEach((thumbList) => {
+        thumbList.classList.add("highlight-new-issues")
+    })
+
+    document.querySelectorAll("ul.thumblist li").forEach(function (item) {
+        let comicTitle = item.querySelector(".detail h5").textContent
+        let comicTitleWords = comicTitle.split(" ")
+        let issueNumbers = comicTitleWords.filter((word) => {
+            //return word.startsWith("#0") || word.startsWith("#1")
+            if (word === "#0" || word === "#1") {
+                return true
+            } else if (word.startsWith("#0") || word.startsWith("#1")) {
+                return /\D/.test(word[2])
+            } else {
+                return false
+            }
+        })
+
+        if (issueNumbers.length >= 1) {
+            console.debug(issueNumbers)
+            console.log("Found new comic: " + comicTitle)
+            item.classList.add("new-issue")
         } else {
-            return false
+            console.warn("Hide existing series: " + comicTitle)
+            item.classList.add("existing-series")
         }
     })
-    
-    if (issueNumbers.length >= 1) {
-        console.debug(issueNumbers)
-        console.log("Found new comic: " + comicTitle)
-    } else {
-        console.warn("Hide existing series: " + comicTitle)
-        item.style.display = "none"
-    }
-})
+}
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,3 +1,10 @@
 import { sayHello } from "./greet";
+import { highlightNewIssues } from "./highlight-new-issues";
 
 console.log(sayHello("TypeScript"));
+
+declare global {
+    interface Window { highlightNewIssues: any; }
+}
+
+window.highlightNewIssues = highlightNewIssues;
