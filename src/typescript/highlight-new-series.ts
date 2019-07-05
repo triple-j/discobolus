000ab5edbf5c4d90d4fb2e2e8fa06a23c5d8310d
Jeremie J. Jarosh <jeremie@jarosh.org>
2019-07-05 13:34:03 -0500

    Convert `HighlightNewSeries` into a class

--- src/typescript/highlight-new-series.ts
+++ src/typescript/highlight-new-series.ts
@@ -6,86 +6,100 @@ interface HTMLInputEvent extends Event {
     target: HTMLInputElement & EventTarget
 }
 
-function settingChanged(event: HTMLInputEvent) {
-    if (event.target.checked) {
-        console.log('checked')
-        Cookie.set(COOKIE_NAME, "yes", {path: "/"})
-        highlightNewSeries()
-    } else {
-        console.log('not checked')
-        Cookie.set(COOKIE_NAME, "no", {path: "/"})
+export class HighlightNewSeries {
+    get enabled(): boolean {
+        return Cookie.get(COOKIE_NAME) == "yes"
+    }
+
+    set enabled(enable: boolean) {
+        let value = enable ? "yes" : "no"
+        Cookie.set(COOKIE_NAME, value, {path: "/"})
+    }
+
+    constructor() {
+        let controlStripElm = document.querySelector(".controlstrip")
+        let isProductPage = Boolean(controlStripElm)
+        let isEnabled = this.enabled
+
+        // This binding is necessary to make `this` work in the callback
+        this.settingChanged = this.settingChanged.bind(this)
+
+        if ( isProductPage ) {
+            // append dom
+            let inputName = "highlightnewseries"
+            let wrapper = document.createElement("span")
+            let checkbox = document.createElement("input")
+            let label = document.createElement("label")
+
+            wrapper.classList.add("highlight-new-series-control")
+
+            checkbox.setAttribute("id", inputName)
+            checkbox.setAttribute("name", inputName)
+            checkbox.setAttribute("type", "checkbox")
+            checkbox.setAttribute("value", "yes")
+            if ( isEnabled ) {
+                checkbox.checked = true
+            }
+            checkbox.addEventListener("change", this.settingChanged)
+
+            label.setAttribute("for", inputName)
+            label.innerHTML = "Show New #1s"
+
+            wrapper.appendChild(label)
+            wrapper.appendChild(checkbox)
+
+            controlStripElm.querySelector(".float-right").prepend(wrapper)
+        }
+
+        if ( isProductPage && isEnabled ) {
+            this.highlightNewSeries()
+        }
+    }
+
+    settingChanged(event: HTMLInputEvent) {
+        if (event.target.checked) {
+            console.log('checked')
+            this.enabled = true
+            this.highlightNewSeries()
+        } else {
+            console.log('not checked')
+            this.enabled = false
+
+            document.querySelectorAll("ul.thumblist").forEach((thumbList) => {
+                thumbList.classList.remove("highlight-new-series")
+            })
+        }
+    }
+
+    highlightNewSeries() {
+        // TODO: apply to `TextView` as well
 
         document.querySelectorAll("ul.thumblist").forEach((thumbList) => {
-            thumbList.classList.remove("highlight-new-series")
+            thumbList.classList.add("highlight-new-series")
         })
-    }
-}
 
-export function highlightNewSeries() {
-    // TODO: apply to `TextView` as well
+        document.querySelectorAll("ul.thumblist li").forEach(function (item) {
+            let comicTitle = item.querySelector(".detail h5").textContent
+            let comicTitleWords = comicTitle.split(" ")
+            let issueNumbers = comicTitleWords.filter((word) => {
+                //return word.startsWith("#0") || word.startsWith("#1")
+                if (word === "#0" || word === "#1") {
+                    return true
+                } else if (word.startsWith("#0") || word.startsWith("#1")) {
+                    return /\D/.test(word[2])
+                } else {
+                    return false
+                }
+            })
 
-    document.querySelectorAll("ul.thumblist").forEach((thumbList) => {
-        thumbList.classList.add("highlight-new-series")
-    })
-
-    document.querySelectorAll("ul.thumblist li").forEach(function (item) {
-        let comicTitle = item.querySelector(".detail h5").textContent
-        let comicTitleWords = comicTitle.split(" ")
-        let issueNumbers = comicTitleWords.filter((word) => {
-            //return word.startsWith("#0") || word.startsWith("#1")
-            if (word === "#0" || word === "#1") {
-                return true
-            } else if (word.startsWith("#0") || word.startsWith("#1")) {
-                return /\D/.test(word[2])
+            if (issueNumbers.length >= 1) {
+                console.debug(issueNumbers)
+                console.log("Found new comic: " + comicTitle)
+                item.classList.add("new-series")
             } else {
-                return false
+                console.warn("Hide existing series: " + comicTitle)
+                item.classList.add("existing-series")
             }
         })
-
-        if (issueNumbers.length >= 1) {
-            console.debug(issueNumbers)
-            console.log("Found new comic: " + comicTitle)
-            item.classList.add("new-series")
-        } else {
-            console.warn("Hide existing series: " + comicTitle)
-            item.classList.add("existing-series")
-        }
-    })
-}
-
-export function initialize() {
-    let controlStripElm = document.querySelector(".controlstrip")
-    let isProductPage = Boolean(controlStripElm)
-    let isEnabled = Cookie.get(COOKIE_NAME) == "yes"
-
-    if ( isProductPage ) {
-        // append dom
-        let inputName = "highlightnewseries"
-        let wrapper = document.createElement("span")
-        let checkbox = document.createElement("input")
-        let label = document.createElement("label")
-
-        wrapper.classList.add("highlight-new-series-control")
-
-        checkbox.setAttribute("id", inputName)
-        checkbox.setAttribute("name", inputName)
-        checkbox.setAttribute("type", "checkbox")
-        checkbox.setAttribute("value", "yes")
-        if ( isEnabled ) {
-            checkbox.checked = true
-        }
-        checkbox.addEventListener("change", settingChanged)
-
-        label.setAttribute("for", inputName)
-        label.innerHTML = "Show New #1s"
-
-        wrapper.appendChild(label)
-        wrapper.appendChild(checkbox)
-
-        controlStripElm.querySelector(".float-right").prepend(wrapper)
-    }
-
-    if ( isProductPage && isEnabled ) {
-        highlightNewSeries()
     }
 }
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,14 +1,13 @@
 import { sayHello } from "./greet"
-import { highlightNewSeries } from "./highlight-new-series"
-import { initialize as highlightNewSeriesInit } from "./highlight-new-series"
 import { Cookie } from "./cookie"
+import { HighlightNewSeries } from "./highlight-new-series"
 import { InfiniteScroll } from "./infinite-scroll"
 import { OrderHistoryStatus } from "./order-history-status"
 import { SettingsPage } from "./settings-page"
 
 
 console.log(sayHello("TypeScript"))
-highlightNewSeriesInit()
+let highlightNewSeries = new HighlightNewSeries()
 let infiniteScroll = new InfiniteScroll()
 let orderHistoryStatus = new OrderHistoryStatus()
 let settingsPage = new SettingsPage()
