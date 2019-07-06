51beb65659f6a516832a699b836edaa06e1f1915
Jeremie J. Jarosh <jeremie@jarosh.org>
2019-07-06 12:42:30 -0500

    Disable the Settings page as it is currently unused

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -3,14 +3,14 @@ import { Cookie } from "./cookie"
 import { HighlightNewSeries } from "./highlight-new-series"
 import { InfiniteScroll } from "./infinite-scroll"
 import { OrderHistoryStatus } from "./order-history-status"
-import { SettingsPage } from "./settings-page"
+//import { SettingsPage } from "./settings-page"
 
 
 console.log(sayHello("TypeScript"))
 let highlightNewSeries = new HighlightNewSeries()
 let infiniteScroll = new InfiniteScroll()
 let orderHistoryStatus = new OrderHistoryStatus()
-let settingsPage = new SettingsPage()
+//let settingsPage = new SettingsPage()
 
 /* -------------------------------------------------------------------------- */
 
@@ -25,4 +25,4 @@ window.discobolus.highlightNewSeries = highlightNewSeries
 window.discobolus.Cookie = Cookie
 window.discobolus.infiniteScroll = infiniteScroll
 window.discobolus.orderHistoryStatus = orderHistoryStatus
-window.discobolus.settingsPage = settingsPage
+//window.discobolus.settingsPage = settingsPage
